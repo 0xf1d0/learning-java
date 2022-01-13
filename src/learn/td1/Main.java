b5749245a0847c3d1ce4b015c39e066bc74f8d81
Phindau <40890393+Phindau@users.noreply.github.com>
2022-01-14 00:30:41 +0100

    Update Main.java

--- src/learn/td1/Main.java
+++ src/learn/td1/Main.java
@@ -12,7 +12,7 @@ public class Main {
      */
     public static void main(String[] args) {
         Student vincent;
-        vcohadon = new Student(213231, "Vincent", "C");
+        vincent = new Student(213231, "Vincent", "C");
 
         Course course1, course2;
         course1 = new Course("Java", 2);
