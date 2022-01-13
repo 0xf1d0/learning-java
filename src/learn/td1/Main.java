ec4fbe440fa99f50958dbbd44433817bc76bff22
Phindau <40890393+Phindau@users.noreply.github.com>
2022-01-14 00:30:23 +0100

    Update Main.java

--- src/learn/td1/Main.java
+++ src/learn/td1/Main.java
@@ -11,15 +11,15 @@ public class Main {
      * @param args Arguments passed while executing the app
      */
     public static void main(String[] args) {
-        Student vcohadon;
-        vcohadon = new Student(213231, "Vincent", "COHADON");
+        Student vincent;
+        vcohadon = new Student(213231, "Vincent", "C");
 
         Course course1, course2;
         course1 = new Course("Java", 2);
         course2 = new Course("AlgoTab", 3);
 
         // Append the courses to the student
-        vcohadon.addCourses(course1, course2);
-        System.out.println(vcohadon);
+        vincent.addCourses(course1, course2);
+        System.out.println(vincent);
     }
 }
